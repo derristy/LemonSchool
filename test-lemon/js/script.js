85c1b0f7239925543a9f916b8646bc609ee057a7
Derristy <derristy@gmail.com>
2022-12-05 13:46:54 +0200

    Commit

--- test-lemon/js/script.js
+++ test-lemon/js/script.js
@@ -1,29 +1,18 @@
-const user_cards = {
-    "homework-rostislav_suslov" : "HOMEWORK/dev-RostislavSuslov/user.html",
-    "homework-dmitry_shumskiy" : "HOMEWORK/dev-DmitryShumskiy/user.html",
-    "homework-kirill_shkaev" : "HOMEWORK/dev-KirillShkaev/user.html",
-};
-  
-for (const [key, value] of Object.entries(user_cards)) {
-    load_card(key, value);
-}
-
-function load_card(user_id, user_url){
-
-    let element = $("#" + user_id);
+$("[data-url]").each(function(){
+    let element = $(this);
 
     $.ajax({
         beforeSend: function(){
             element.html("<div class=\"spinner-border mt-3\" role=\"status\"><span class=\"visually-hidden\">Loading...</span></div>");
         },
-        url: user_url,
+        url: element.attr("data-url"),
         dataType: "html",
-        cache: true,
+        cache: false,
         success: function(data){
             element.html(data);
         },
         error: function(){
-            element.html("<div class=\"alert alert-danger mt-3\" role=\"alert\">Homework load failed</div>")
+            element.html("<div class=\"alert alert-danger mt-3\" role=\"alert\">Homework load failed</div>");
         }
     });
-}
+});
