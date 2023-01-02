590bee14282db68a82951c7051f67a8c0470ccac
Derristy <derristy@gmail.com>
2023-01-02 20:34:07 +0200

    Lesson-17 delivery-slider

--- lesson-17/assets/js/app.js
+++ lesson-17/assets/js/app.js
@@ -11,40 +11,12 @@ $(document).ready(() => {
     });
 
     $('.delivery-slider').slick({
-        dots: true,
-        infinite: false,
+        dots: false,
+        arrows: true,
+        infinite: true,
         speed: 300,
         slidesToShow: 3,
-        slidesToScroll: 1,
-        variableWidth: true,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ]
+        slidesToScroll: 3,
       });
 
     advantages_slider();
