232c012a576887686ebb52bd8c1edbe228a58539
Derristy <derristy@gmail.com>
2023-01-24 18:39:53 +0200

    Lesson-21

--- lesson-21/dev/assets/js/app.js
+++ lesson-21/dev/assets/js/app.js
@@ -1,3 +1,17 @@
+window.addEventListener("DOMContentLoaded", () => {
+    let modal = document.querySelectorAll(".modal");
+
+    modal.forEach((element) => {
+        element.addEventListener("click", (event) => {
+
+            if(event.target.classList.contains("modal")){
+                modalClose(event.target.id);
+            }
+        });
+    });
+
+});
+
 const navbar_burger = document.querySelector(".navbar-burger");
 const navbar_burger_item = document.querySelector(".navbar-burger__item");
 const navbar_menu = document.querySelector(".navbar-menu");
@@ -5,6 +19,20 @@ const phone_dropdown__main = document.querySelector(".phone-dropdown__main");
 const phone_dropdown__btn = document.querySelector(".phone-dropdown__btn");
 const phone_dropdown__content = document.querySelector(".phone-dropdown__content");
 const border_example = document.querySelector(".border-example");
+
+const modalShow = (modalID) => {
+    let element = document.querySelector("#"+modalID);
+
+    element.classList.remove("hide");
+    element.classList.add("show");
+};
+
+const modalClose = (modalID) => {
+    let element = document.querySelector("#"+modalID);
+
+    element.classList.add("hide");
+    element.classList.remove("show");
+};
 
 navbar_burger.addEventListener("click", () => {
     navbar_burger_item.classList.toggle("active");
