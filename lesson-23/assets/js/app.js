7cf3f8e6008987779c1b9ec6a92aa1a92dc56112
Derristy <derristy@gmail.com>
2023-01-28 14:01:44 +0200

    Lesson-23

--- lesson-23/assets/js/app.js
+++ lesson-23/assets/js/app.js
@@ -1,11 +1,17 @@
 const floor = document.querySelectorAll(".floor");
+let floors = document.querySelector(".floors span");
+let rooms = document.querySelector(".rooms span");
 
 floor.forEach(element => {
     element.addEventListener("mouseover", () => {
-        element.classList.add("fill");
+        // element.classList.add("fill");
+        floors.innerHTML = element.getAttribute("data-floors");
+        rooms.innerHTML = element.getAttribute("data-rooms");
     });
 
     element.addEventListener("mouseout", () => {
-        element.classList.remove("fill");
+        // element.classList.remove("fill");
+        floors.innerHTML = 0;
+        rooms.innerHTML = 0;
     });
 });
