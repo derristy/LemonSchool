d5caa6a34f8b3cdd31078cbf0c58beb52f3e5c26
Derristy <derristy@gmail.com>
2023-01-28 14:10:32 +0200

    Lesson-23

--- lesson-23/assets/js/app.js
+++ lesson-23/assets/js/app.js
@@ -4,13 +4,11 @@ let rooms = document.querySelector(".rooms span");
 
 floor.forEach(element => {
     element.addEventListener("mouseover", () => {
-        // element.classList.add("fill");
         floors.innerHTML = element.getAttribute("data-floors");
         rooms.innerHTML = element.getAttribute("data-rooms");
     });
 
     element.addEventListener("mouseout", () => {
-        // element.classList.remove("fill");
         floors.innerHTML = 0;
         rooms.innerHTML = 0;
     });
